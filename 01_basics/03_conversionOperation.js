7a2d7b564500f894a21868af5e26f8d4aca3ee00
Ishwor Amgai <58836618+Eswor0830@users.noreply.github.com>
2025-03-21 05:46:39 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "Ishwor"
+
+//console.log (typeof score);
+//console.log (typeof (score));
+
+let valueInNumber = Number(score)
+//console.log (typeof valueInNumber);
+//console.log (valueInNumber);
+
+// "33" => number 
+// "33abc" => NaN   
+// "True" => 1
+// "False" => 0
+
+let isLoggedIn = "Ishwor" 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 =>false
+// " " => false
+// "Ishwor" =>true
+
+let someNumber = 33 
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
